14ed5e18424ad41e101e3da0cc324b1215f22534
Nhật Nguyên Đặng <nguyen.dangn@one-line.com>
2021-10-26 15:58:46 +0700

    Fix and combine all function

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-// import { showData, handleShowData } from "./modules/readData.js";
-// import { updateData } from "./modules/updateData.js";
-// import { checkDataDelete, deleteData } from "./modules/deleteData.js";
-// import { saveData } from "./modules/saveData.js";
-// import { addData } from "./modules/addData.js";
+const inputTextIdClass = document.querySelector("#id-add");
+const inputTextNameClass = document.querySelector("#class-name-add");
+const textWarning = document.querySelector("#text-warning");
+const btnCancel = document.querySelector("#btn-cancel-add");
+const btnClose = document.querySelector("#btn-close-add");
+const btnDel = document.querySelector(".delete-btn");
+const btnSubmit = document.querySelector("#btn-submit-add");
+const btnsCancelModal = document.querySelectorAll(".btn--cancel-modal");
+const btnsCloseModal = document.querySelectorAll(".btn--close-modal");
+const modalDel = document.querySelector("#delete-modal");
+const modalAdd = document.querySelector("#add-modal");
+const modal = document.querySelector("#add-modal");
 
-let btnsCancelModal = document.querySelectorAll(".btn--cancel-modal");
-let btnsCloseModal = document.querySelectorAll(".btn--close-modal");
+localStorage.setItem("checkAddDuplicateData", "false");
 
 let classrooms = [
   {
@@ -42,7 +48,8 @@ let students = [
     address: "k7/14 Pasteur",
   },
 ];
-
+let classroomsDataLocalStorage = saveData(classrooms, "classrooms");
+let studentDataLocalStorage = saveData(students, "students");
 function saveData(listObject, keyName) {
   let classroomsData;
   if (localStorage.getItem(keyName) == null) {
@@ -54,48 +61,41 @@ function saveData(listObject, keyName) {
   return classroomsData;
 }
 
-let classroomsDataLocalStorage = saveData(classrooms, "classrooms");
-localStorage.setItem("checkAddDuplicateData", "false");
-
-// showData(classroomsDataLocalStorage);
+// Show data
 
 function showData(listObjects) {
-  if (!listObjects.length) return; // check if listObject is empty
-  var keyObject = Object.keys(listObjects[0]);
-  var table = document.querySelector("tbody");
+  if (!listObjects.length) return;
+  const keyObject = Object.keys(listObjects[0]);
+  const table = document.querySelector("tbody");
   listObjects.forEach(function (object) {
-    // Create new row
-    var newRow = document.createElement("tr");
+    const newRow = document.createElement("tr");
     newRow.setAttribute("class-id", object[keyObject[0]]);
 
-    // Create new column base object
     handleShowData(object, keyObject, newRow);
 
-    // Add column checkbox
     handleShowCheckbox(newRow);
-    // Add column button edit
-    handleShowBtnEdit(newRow, object[keyObject[0]]);
 
+    handleShowBtnEdit(newRow, object[keyObject[0]]);
+    newRow.setAttribute("onclick", `redirect(${object[keyObject[0]]})`);
     table.appendChild(newRow);
   });
 }
 
-// Add data into table
 function handleShowData(object, keyObject, newRow) {
   keyObject.forEach(function (key) {
-    var newCell = document.createElement("th");
+    const newCell = document.createElement("th");
     newCell.setAttribute("scope", "col");
-    var value = document.createTextNode(object[key]);
+    const value = document.createTextNode(object[key]);
     newCell.appendChild(value);
 
     newRow.appendChild(newCell);
   });
 }
-// Add chekcbox into table
+
 function handleShowCheckbox(newRow) {
-  var cellCheckbox = document.createElement("th");
+  const cellCheckbox = document.createElement("th");
   cellCheckbox.setAttribute("scope", "col");
-  var checkbox = document.createElement("INPUT");
+  const checkbox = document.createElement("INPUT");
   checkbox.setAttribute("type", "checkbox");
   checkbox.classList.add("form-check-input");
 
@@ -103,12 +103,12 @@ function handleShowCheckbox(newRow) {
 
   newRow.appendChild(cellCheckbox);
 }
-// Add edit button into table
+
 function handleShowBtnEdit(newRow, idClass) {
-  var cellBtnEdit = document.createElement("th");
+  const cellBtnEdit = document.createElement("th");
   cellBtnEdit.setAttribute("scope", "col");
-  var btnEdit = document.createElement("button");
-  var newBtnText = document.createTextNode("Edit");
+  const btnEdit = document.createElement("button");
+  const newBtnText = document.createTextNode("Edit");
   btnEdit.appendChild(newBtnText);
   btnEdit.classList.add("edit-btn");
   btnEdit.setAttribute("onclick", `updateData(${idClass})`);
@@ -117,9 +117,10 @@ function handleShowBtnEdit(newRow, idClass) {
   newRow.appendChild(cellBtnEdit);
 }
 
-showData(classroomsDataLocalStorage);
-
-// updateData(classroomsDataLocalStorage);
+function showAddModal() {
+  modalAdd.style.display = "block";
+}
+// Update data
 
 function updateData(idClass) {
   const modalEdit = document.querySelector("#edit-modal");
@@ -127,14 +128,15 @@ function updateData(idClass) {
   const btnSubmitEdit = document.querySelector("#btn-submit-edit");
   const inputTextNameClassModal = document.querySelector("#class-name");
   const tableBody = document.querySelector("tbody");
-  console.log(tableBody.childNodes.length);
+
   const classrooms = JSON.parse(localStorage.getItem("classrooms"));
   const idxClassroom = classrooms.findIndex(
     (classroom) => classroom.idClass == idClass
   );
-  console.log(idxClassroom);
-  const cellNameClass = tableBody.childNodes[idxClassroom].childNodes[1];
 
+  const row = tableBody.childNodes[idxClassroom];
+
+  const cellNameClass = row.childNodes[1];
   inputTextNameClassModal.value = classrooms[idxClassroom].nameClass;
 
   modalEdit.style.display = "block";
@@ -149,46 +151,41 @@ function updateData(idClass) {
   };
 }
 
-const modal = document.querySelector("#add-modal");
-const btnSubmit = document.querySelector("#btn-submit-add");
-const inputTextIdClass = document.querySelector("#id-add");
-const inputTextNameClass = document.querySelector("#class-name-add");
-const textWarning = document.querySelector("#text-warning");
-const btnCancel = document.querySelector("#btn-cancel-add");
-const btnClose = document.querySelector("#btn-close-add");
+// Add data
 
-function addData(classrooms) {
+function addData() {
   btnSubmit.addEventListener("click", () => {
     const idClass = +inputTextIdClass.value;
     const nameClass = inputTextNameClass.value;
-    console.log(inputTextIdClass.value, inputTextNameClass.value);
     let listNewObjects = [];
+    let classrooms = JSON.parse(localStorage.getItem("classrooms"));
+    if (idClass !== 0 && nameClass !== "") {
+      const isMatch = classrooms.find(
+        (classroom) => classroom.idClass == idClass
+      );
 
-    const isMatch = classrooms.find(
-      (classroom) => classroom.idClass == idClass
-    );
+      if (isMatch) {
+        if (localStorage.getItem("checkAddDuplicateData") == "false") {
+          textWarning.innerHTML = " * * * Please type another ID";
 
-    if (isMatch) {
-      if (localStorage.getItem("checkAddDuplicateData") == "false") {
-        textWarning.innerHTML = " * * * Please type another ID";
-        console.log("abc");
-        localStorage.setItem("checkAddDuplicateData", "true");
+          localStorage.setItem("checkAddDuplicateData", "true");
+        }
+      } else {
+        const newClass = {
+          idClass,
+          nameClass,
+        };
+
+        listNewObjects.push(newClass);
+        classrooms.push(newClass);
+
+        modal.style.display = "none";
+        localStorage.setItem("classrooms", JSON.stringify(classrooms));
+        resetForm();
+        showData(listNewObjects);
       }
     } else {
-      const newClass = {
-        idClass,
-        nameClass,
-      };
-
-      listNewObjects.push(newClass);
-      classrooms.push(newClass);
-
-      modal.style.display = "none";
-
-      resetForm();
-      showData(listNewObjects);
-      updateData(listNewObjects);
-      localStorage.setItem("classrooms", JSON.stringify(classrooms));
+      textWarning.innerHTML = " * * * Please type full information";
     }
   });
 
@@ -215,77 +212,111 @@ function resetForm() {
   inputTextIdClass.value = "";
   inputTextNameClass.value = "";
 }
-addData(classroomsDataLocalStorage);
 
 // Delete data
-const btnAll = document.querySelector(".select-all-btn");
-const checkboxs = document.querySelectorAll(".form-check-input");
-btnAll.onclick = function () {
-  btnAll.textContent === "Select All"
-    ? (btnAll.textContent = "Cancel All")
-    : (btnAll.textContent = "Select All");
+const btnSelectAll = document.querySelector(".select-all-btn");
+btnSelectAll.addEventListener("click", selectAll);
+function selectAll() {
+  const checkBoxs = document.querySelectorAll(".form-check-input");
 
-  checkboxs.forEach(function (checkbox) {
-    if (btnAll.textContent === "Cancel All") checkbox.checked = true;
+  this.textContent === "Select All"
+    ? (this.textContent = "Cancel All")
+    : (this.textContent = "Select All");
+
+  checkBoxs.forEach((checkbox) => {
+    if (this.textContent === "Cancel All") checkbox.checked = true;
     else checkbox.checked = false;
   });
-};
+}
 
-// handle button modal close button
-btnsCloseModal.forEach(function (closeBtn) {
-  closeBtn.onclick = function () {
-    closeBtn.closest(".modal").style.display = "none";
-  };
-});
-
-btnsCancelModal.forEach((btnCancel) => {
-  btnCancel.addEventListener("click", (e) => {
-    btnCancel.closest(".modal").style.display = "none";
-  });
-});
-
-const modalDel = document.querySelector("#delete-modal");
-const btnDel = document.querySelector(".delete-btn");
-btnDel.onclick = function () {
-  modalDel.style.display = "block";
-
+function checkDataDelete(listObject) {
+  let checkBoxs = document.querySelectorAll("input[type = checkbox]");
+  if (checkBoxs) {
+    let delData = [];
+    checkBoxs.forEach(function (checkbox) {
+      if (checkbox.checked == true) {
+        let delRow = checkbox.closest("tr");
+        let rowID = delRow.getAttribute("class-id");
+        let delClass = listObject.find(function (object) {
+          return object.idClass == rowID;
+        });
+        delData.push(delClass);
+      }
+    });
+    return delData;
+  }
+}
+function handleDeleteData() {
   const delMessage = modalDel.querySelector(".modal-body-message");
   const delTable = modalDel.querySelector("tbody");
-  delTable.innerHTML = "";
-  let delData = checkDataDelete(classroomsDataLocalStorageData);
+  const submitBtn = modalDel.querySelector(".btn--submit-modal");
+  let delData = checkDataDelete(JSON.parse(localStorage.getItem("classrooms")));
 
+  modalDel.style.display = "block";
+  delTable.innerHTML = "";
+  console.log(delData);
   if (!delData.length) {
     delMessage.textContent = "Nothing to delete !!!!";
   } else {
-    delMessage.innerText = "Do you sure to delete";
     const keyObject = Object.keys(delData[0]);
+    delMessage.innerText = "Do you sure to delete";
     delData.forEach(function (object) {
       const newRow = document.createElement("tr");
       handleShowData(object, keyObject, newRow);
-
       delTable.appendChild(newRow);
     });
   }
 
-  const submitBtn = modalDel.querySelector(".btn--submit-modal");
   submitBtn.onclick = function () {
-    let newClassroom = deleteData(classroomsDataLocalStorageData);
+    let newClassroom = deleteData(
+      JSON.parse(localStorage.getItem("classrooms"))
+    );
     localStorage.setItem("classrooms", JSON.stringify(newClassroom));
     modalDel.style.display = "none";
   };
-};
+}
 
-//handle show student in a class (redirect student page)
-let classRows = document.querySelectorAll("tbody tr");
-classRows.forEach(function (row) {
-  row.onclick = (e) => {
-    if (
-      !e.target.closest('input[type = "checkbox"]') &&
-      !e.target.closest(".edit-btn")
-    ) {
-      console.log(row.getAttribute("class-id"));
-      const idClass = row.getAttribute("class-id");
-      window.location.href = `./student.html?class=${idClass}`;
-    }
-  };
-});
+function deleteData(listObject) {
+  let checkBoxs = document.querySelectorAll("input[type = checkbox]");
+  if (checkBoxs) {
+    checkBoxs.forEach(function (checkbox) {
+      if (checkbox.checked == true) {
+        let delRow = checkbox.closest("tr");
+        let rowID = delRow.getAttribute("class-id");
+        let delClass = listObject.find(function (object) {
+          return object.idClass == rowID;
+        });
+        listObject.splice(listObject.indexOf(delClass), 1);
+        delRow.remove();
+      }
+    });
+    return listObject;
+  }
+}
+
+// Handle event close modal
+function closeModal() {
+  btnsCloseModal.forEach(function (closeBtn) {
+    closeBtn.onclick = function () {
+      closeBtn.closest(".modal").style.display = "none";
+    };
+  });
+
+  btnsCancelModal.forEach((btnCancel) => {
+    btnCancel.addEventListener("click", (e) => {
+      btnCancel.closest(".modal").style.display = "none";
+    });
+  });
+}
+
+function redirect(idClass) {
+  const row = document.querySelector(`tr[class-id="${idClass}"]`);
+
+  // if (!row.matches('input[type = "checkbox"]') && !row.matches(".edit-btn")) {
+  //   window.location.href = `./student.html?class=${idClass}`;
+  // }
+}
+
+showData(classroomsDataLocalStorage);
+addData();
+closeModal();
